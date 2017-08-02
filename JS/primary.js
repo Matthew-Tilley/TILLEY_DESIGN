411ff426e5f4103690f829c97834e825c5fa43b7
Matt Tilley <mtilley255@gmail.com>
2017-08-02 12:03:31 -0500

    Initial upload
    First upload of all the stuff in various project-related folders.

---
+++ JS/primary.js
@@ -0,0 +1,43 @@
+$(document).ready(function()
+{
+	$("#INT01_H201").fadeIn(500)
+					.delay(1000)
+					.fadeOut(100);	
+	
+	$("#INT01_H202").delay(2000)
+					.fadeIn(500)
+					.delay(1000)
+					.fadeOut(100);
+	
+	$("#INT01_H203").delay(4000)
+					.fadeIn(500)
+					.delay(1000)
+					.fadeOut(100);
+	
+	$("#spacer").delay(6000)
+				.css("height", "70vh");
+	
+	$("#INT01_LOG01").delay(6000)
+					.fadeIn(1000);
+	
+	$("#INT01_H204").delay(6000)
+					.fadeIn(2750);
+	
+	$("#SEC01")	.delay(10000)
+			
+				.fadeOut(1000);
+	
+	$("#SEC02")	.delay(11000)
+				.fadeIn(2000);
+	
+	$("#SEC03")	.delay(12000)
+				.fadeIn(1500);
+	
+	$("#SEC04")	.delay(13000)
+				.fadeIn(1500);
+	
+	$("#BTN01")	.delay(14000)
+				.fadeIn(1500)	;
+	
+	
+});
